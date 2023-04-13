2ce1c8ac1a3904b46648d35a1c00eed5e0f39d28
parkkhee <khp963@naver.com>
2023-04-13 16:47:26 +0900

    JPA 함수에서 (을)를 사용하면 관련 엔티티의 속성을 조건으로 검색할 수 있다.

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
@@ -13,6 +13,9 @@ public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, 
     List<LikeablePerson> findByFromInstaMemberId(Long fromInstaMemberId);
     Optional<LikeablePerson> findByFromInstaMemberIdAndToInstaMemberId(Long fromInstaMemberId, Long toInstaMemberId);
 
+    List<LikeablePerson> findByToInstaMember_username(String username); //이런식으로도 호출가능
+    LikeablePerson findByFromInstaMemberIdAndToInstaMember_username(long fromInstaMemberId, String username);//강사님 코드
+
     @Modifying(clearAutomatically = true)
     @Query("update LikeablePerson l set l.attractiveTypeCode = :attractiveTypeCode where l.id = :id")
     void updateByLikeablePersonId(@Param("id")Long id, @Param("attractiveTypeCode")int attractiveTypeCode);
